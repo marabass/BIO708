46937bdeb794baf5d4403f31e50308f1b5c1dbbc
Mariam Abass <abassm2@mcmaster.ca>
2024-03-25 22:13:55 -0400

    GLM script - more edits

--- GLM_MA_Assign.R
+++ GLM_MA_Assign.R
@@ -2,6 +2,10 @@
 
 #loading libraries 
 library(ggplot2); theme_set(theme_bw())
+library(DHARMa)
+library(dotwhisker)
+library(emmeans)
+
 #data set 
 D_SSD_Cond <- readRDS("SpeciesStarvation_Cleaned_MA.rds")
 
@@ -9,27 +13,26 @@ D_SSD_Cond <- readRDS("SpeciesStarvation_Cleaned_MA.rds")
 Dpro_size <- D_SSD_Cond[D_SSD_Cond$species_full =="D_prolongata",]
                         
 #I am modelling the effect of thorax length (a continuous predictor) on tibia length: thorax_length_mm ~ leg_tibL
-#I am using family=gaussian because my response variable is continuous. 
-#Should probably understand what the log link function does here 
-#Log link function performs a log transformation 
-glm1 <- glm(leg_tibL ~ thorax_length_mm + sex, data = Dpro_size, family = gaussian(link="log"))
+#I am using family=gaussian because 
+#I am converting milimeters to micrometers, then performing a log2 transformation of my continuous response and predictor
+#I am using a standard Log link function as per the assumption that my data is normally distributed. 
+#The log link function performs a log transformation of my predictor
 
-glm1_plot <- (ggplot(Dpro_size,aes(thorax_length_mm,leg_tibL, group = sex)) +
+glm1 <- glm(log2(1000 * Dpro_size$leg_tibL) ~ log2(1000 * thorax_length_mm) + sex, data = Dpro_size, family = gaussian(link="log"))
+
+glm1_plot <- (ggplot(Dpro_size,aes(log2(1000 * thorax_length_mm),log2(1000 * Dpro_size$leg_tibL), group = sex, colour = sex)) +
   geom_point() + 
   geom_smooth(method="glm",colour="red",
                          formula=y~x,
                          method.args=list(family="gaussian")))
+print(glm1_plot)
 #diagnostics 
 plot(glm1)
-plot(lm1)
 
-#The curve in the residuals vs fitted plot suggests that it might be best to model the effect of thorax length on tibia length 
-#using a quadratic distribution 
-
-glm2 <- update(glm1,.~poly(thorax_length_mm,2))
-plot(glm2)
-
-#log2 trasnformation? 
+plot(simulateResiduals(glm1))
 
 #coefficient plot 
-summary(m1)
+summary(glm1)
+dwplot(glm1)
+plot(emmeans(glm1, specs = "sex", type = "response"))
+
